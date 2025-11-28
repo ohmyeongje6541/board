75add955f4b7e2a538dc52d0f38d3d469b9de9ef
ohmyeongje <tiqmtiqm6541@gmail.com>
2025-11-28 14:06:28 +0900

    포스트 로직

--- src/main/java/org/example/board/repository/PostRepository.java
+++ src/main/java/org/example/board/repository/PostRepository.java
@@ -6,6 +6,7 @@ import org.example.board.entity.Post;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Slice;
+import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
@@ -98,4 +99,12 @@ public interface PostRepository extends JpaRepository<Post, Long> {
 
     Slice<Post> findAllBy(Pageable pageable);
 
+
+    // findAll() => Jpa 자동생성
+    @Query("SELECT DISTINCT p FROM Post p LEFT JOIN FETCH p.comments ")
+    List<Post> findAllWithComments();
+
+    @EntityGraph(attributePaths = {"comments"})
+    @Query("SELECT p FROM Post p")
+    List<Post> findAllWithCommentsEntityGraph();
 }
